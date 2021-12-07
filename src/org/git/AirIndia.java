1b72a9ece828da60091218649e4a0040717f2671
rekhalogumani <rekhaecemec@gmail.com>
2021-12-07 13:53:38 +0530

    password completed

--- src/org/git/AirIndia.java
+++ src/org/git/AirIndia.java
@@ -1,5 +1,12 @@
 package org.git;
 
 public class AirIndia {
+	public static void password() {
+System.out.println("password completed");
+	}
+	public static void main(String[] args) {
+		password();
+		
+	}
 
 }
